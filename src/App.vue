471994f4252c460e1838b280aba869ceea6d22eb
iiy4383 <znlspf123@naver.com>
2021-06-16 20:29:27 +0900

    vue tutorial 20210616

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CanYouSeeMe v-if="visible" message="can you see me now?" />
+    <Button v-on:click="visible = !visible">Toggle</Button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CanYouSeeMe from './components/CanYouSeeMe.vue'
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      visible: true
+    }
+  },
+  components: {
+    HelloWorld,
+    CanYouSeeMe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CanYouSeeMe.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="CanYouSeeMe" v-if="CanYouSeeMe">
+        {{ message }}
+    </div>
+</template>
+<script>
+export default {
+    name: 'CanYouSeeMe',
+    props: {
+        message: String
+    },
+    data: function() {
+        return {
+                CanYouSeeMe: true
+            };
+    }
+}
+</script>
+<style>
+    
+</style>
