86da8a842b91fd118c4eb63b081c7a065fab27b9
iiy4383 <znlspf123@naver.com>
2021-06-19 11:34:15 +0900

    20210619 punchbag game!

--- README.md
+++ README.md
@@ -10,31 +10,6 @@ Vue를 처음 공부하기 시작했습니다.
 갈 길이 멀고 늦은 감도 있고, Svelte를 선수 학습하고 Vue를 하는 이상한 느낌도 있지만
 그래도 한 번 최대한 공부해보려 합니다.
 
-## 20210619
-v-if과 v-show를 익혀 조건적으로 컴포넌트를 나타나게 하는 방법을 익혔습니다.
-v-elseif는 있는데 v-else는 안되네요..
-Memo : "v-if는 정말 조건적 그 자체지만, v-show는 해당 태그의 style에 display: none을 적용한다"
-v-for를 이용하여 컴포넌트를 만드는 방법을 알았고, template 태그가 무슨 역할인지 이제 안 것 같습니다.
-React에서 사용하는 <> </> 와 같다고 느껴지네요.
-
-## 20210618
-React의 Hook인 useState를 이용한 Data Binding 방법과 유사하게
-v-model을 사용하는 방법을 배웠습니다.
-v-bind:class 를 이용해 Dynamic하게 CSS를 사용하는 방법을 배웠습니다.
-
-## 20210617
-v-on:Click.once , v-on:Click.prevent 와 같은
-Event Modifier를 적용해보았습니다.
-
-## 20210616
-
-생각보다 Vue는 굉장히 재밌으면서도,
-React에 친숙했던 저에겐 아주 낯설게 다가옵니다.
-복잡하지만, 그래도 Vue만의 낯설은 구조가 마냥 재밌습니다.
-더 열심히 공부해보고 싶은 생각이 듭니다.
-기본적인 뼈대를 어느 정도 공부하였으며,
-v- directive를 배웠습니다.
-DOM과의 data binding이 어떻게 이루어지는지 확인하였으며
-Vue만의 색채에 조금씩 적응하려 노력중입니다.
-props, data, method, name, el 등등에 대해 다소 감을 잡았습니다.
-아주 기초적인 수준인 것 같은 느낌이 들지만, 멀지 않아 Vue를 잘 다루게 될 것 같은 느낌도 듭니다.
+## punchbag game
+<a href="https://www.youtube.com/watch?v=WjfpQlVem-8&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=14">이곳</a>에서 가르쳐주는 펀치백게임을 따라 만들어봤습니다!
+6월 19일 기준으로 여태까지 배운 것들을 정리하는데 큰 도움이 되었습니다!
--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,34 @@
 <template>
   <div id="app">
-    <CanYouSeeMe message_one="can you see me now?" message_two="now you don't."/>
-    <Counter />
-    <DontLeaveMe />
-    <DontBotherMe />
-    <NewWayOfDataBinding />
-    <div id="colored_one" v-bind:class="{red:true, blue:false}">
-      <p>hi!</p>
+    <div id="bag_image_container">
     </div>
-    <div id="colored_two" v-bind:class="{dynamicCSS: dynamicCSS}">
-      <button v-on:click="dynamicCSS = !dynamicCSS">click me to toggle color</button>
-      <p>hi there!</p>
-    </div>
-    <button id="toggleShow" v-on:click="show = !show">toggle show!</button>
-    <p v-if="show">show me!</p>
-    <p v-show="show">elseif show me!</p>
-    <ul id="myUl">
-      <p>My hobbies:</p>
-      <li v-for="(hobby, index) of Lee.hobby" v-bind:key="hobby">{{index + 1}}. {{hobby}}</li>
-    </ul>
-    <template v-for="text in template_is_amazing">
-      <span v-bind:key="text">
-        {{text}}
-      </span>
-    </template>
-    <p>Let me introduce myself.</p>
-    <template v-for="(val, key) in Lee">
-      <div v-bind:key="key">
-        {{key}} : {{val}}
+    <div id="bag-healthbar">
+      <div id="bag-healthbar-health" v-bind:style="{ width: health + '%' }">
       </div>
-    </template>
+    </div>
+    <div id="bag-punch_restart">
+      <button v-if="health !== 0" v-on:click.prevent="punch">Punch!</button>
+      <button v-on:click.prevent="restart">Restart</button>
+    </div>
   </div>
 </template>
 
 <script>
-import CanYouSeeMe from './components/CanYouSeeMe.vue'
-import Counter from './components/Counter.vue'
-import DontLeaveMe from './components/DontLeaveMe.vue'
-import DontBotherMe from './components/DontBotherMe.vue'
-import NewWayOfDataBinding from './components/NewWayOfDataBinding.vue'
 
 export default {
   name: 'App',
   data: function () {
     return {
-      dynamicCSS: true,
-      show: true,
-      show_important: true,
-      Lee: {
-        name: "Lee",
-        age: 27,
-        hobby: ["Playing Game", "Singing"],
-        email: "znlspf123@naver.com"
-      },
-      template_is_amazing: ["This", "is", "amazing", "!"]
+      health: 100
     }
   },
-  components: {
-    CanYouSeeMe,
-    Counter,
-    DontLeaveMe,
-    DontBotherMe,
-    NewWayOfDataBinding
+  methods: {
+    punch: function () {
+      this.health -= 5
+    },
+    restart: function () {
+      this.health = 100
+    }
   }
 }
 </script>
@@ -76,22 +43,29 @@ export default {
   margin-top: 60px;
 }
 
-.red {
-  color: red;
+#bag_image_container {
+  width: 25rem;
+  height: 30rem;
+  margin: 0 auto;
+  background: url(assets/punchbag.jpg);
+  background-repeat: no-repeat;
 }
 
-.blue {
-  color: blue;
+#bag-healthbar {
+  margin: 0 auto;
+  width: 20rem;
+  height: 1.5rem;
+  border: 5px solid black;
+  margin: 0 auto 2rem auto;
 }
 
-.dynamicCSS > p {
-  font-size: 1.5rem;
-  color: green;
+#bag-healthbar-health {
+  height: 100%;
+  background-color: red;
 }
 
-#myUl {
-  list-style: none;
-  padding: 0;
+button {
+  margin: 1rem 1rem;
 }
 
 </style>
